9d181243abc44067f51f04f29126b96ce5c2692a
Steven Anderson <happygoatdesigns@gmail.com>
2016-12-05 14:08:58 -0700

    more layout work done and form created on the initial view

---
+++ public/app/directives/nav-top/navTopArrowDir.js
@@ -0,0 +1,10 @@
+angular.module("app")
+.directive('navTopArrow', function() {
+
+  return {
+    restrict: 'AE',
+    templateUrl: './../public/app/directives/nav-top/navTopArrowTmpl.html'
+    }
+
+
+});
